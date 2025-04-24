40d1c8134d51091a7facd102097713f2ff04cd05
karthikroyal5678 <karthikroyal5678@gmail.com>
2025-04-25 00:29:28 +0530

    Add useNutritionData hook for nutrition API integrationnnnmnnnn

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -1,6 +1,5 @@
 export default async (request: Request) => {
-    return new Response("Hello from the edge!", {
-      headers: { "Content-Type": "text/plain" },
-    });
-  };
-  
+  return new Response("Hello from Edge Function!", {
+    headers: { "Content-Type": "text/plain" },
+  });
+};
